6b9d31968a34739f22ff56f20dc2c9925d3ea5ec
Lars Muldjord <muldjordlars@gmail.com>
2022-05-09 23:24:45 +0200

    Translation

--- ezmarket_da_DK.ts
+++ ezmarket_da_DK.ts
@@ -17,85 +17,277 @@
     </message>
 </context>
 <context>
+    <name>AccountEditor</name>
+    <message>
+        <location filename="src/accounteditor.cpp" line="41"/>
+        <source>Barcode: </source>
+        <translation>Stregkode: </translation>
+    </message>
+</context>
+<context>
     <name>AccountWidget</name>
     <message>
-        <location filename="src/accountwidget.cpp" line="36"/>
+        <location filename="src/accountwidget.cpp" line="38"/>
         <source>Account holder:</source>
         <translation>Konto ejer:</translation>
     </message>
     <message>
-        <location filename="src/accountwidget.cpp" line="40"/>
+        <location filename="src/accountwidget.cpp" line="42"/>
         <source>Balance:</source>
         <translation>Indestående:</translation>
+    </message>
+    <message>
+        <location filename="src/accountwidget.cpp" line="46"/>
+        <source>Bonus points:</source>
+        <translation>Bonus point:</translation>
+    </message>
+</context>
+<context>
+    <name>AccountsModel</name>
+    <message>
+        <location filename="src/accountsmodel.cpp" line="130"/>
+        <source>Account</source>
+        <translation>Konto</translation>
+    </message>
+    <message>
+        <location filename="src/accountsmodel.cpp" line="133"/>
+        <source>Balance</source>
+        <translation>Indestående</translation>
+    </message>
+    <message>
+        <location filename="src/accountsmodel.cpp" line="136"/>
+        <source>Bonus</source>
+        <translation>Bonus</translation>
+    </message>
+    <message>
+        <location filename="src/accountsmodel.cpp" line="139"/>
+        <source>Barcode</source>
+        <translation>Stregkode</translation>
+    </message>
+    <message>
+        <location filename="src/accountsmodel.cpp" line="142"/>
+        <source>Unknown</source>
+        <translation>Ukendt</translation>
+    </message>
+</context>
+<context>
+    <name>CategoriesModel</name>
+    <message>
+        <location filename="src/categoriesmodel.cpp" line="127"/>
+        <source>Category</source>
+        <translation>Kategori</translation>
+    </message>
+    <message>
+        <location filename="src/categoriesmodel.cpp" line="130"/>
+        <source>Lifespan</source>
+        <translation>Holdbarhed</translation>
+    </message>
+    <message>
+        <location filename="src/categoriesmodel.cpp" line="133"/>
+        <source>Barcode</source>
+        <translation>Stregkode</translation>
+    </message>
+    <message>
+        <location filename="src/categoriesmodel.cpp" line="136"/>
+        <source>Unknown</source>
+        <translation>Ukendt</translation>
+    </message>
+</context>
+<context>
+    <name>CategoryEditor</name>
+    <message>
+        <location filename="src/categoryeditor.cpp" line="41"/>
+        <source>Barcode: </source>
+        <translation>Stregkode: </translation>
+    </message>
+</context>
+<context>
+    <name>CategoryWidget</name>
+    <message>
+        <location filename="src/categorywidget.cpp" line="38"/>
+        <source>Category name:</source>
+        <translation>Kategori navn:</translation>
+    </message>
+    <message>
+        <location filename="src/categorywidget.cpp" line="42"/>
+        <source>Icon:</source>
+        <translation>Billede:</translation>
+    </message>
+    <message>
+        <location filename="src/categorywidget.cpp" line="44"/>
+        <source>Icon search</source>
+        <translation>Billedsøgning</translation>
+    </message>
+    <message>
+        <location filename="src/categorywidget.cpp" line="49"/>
+        <source>Lifespan:</source>
+        <translation>Holdbarhed:</translation>
+    </message>
+    <message>
+        <location filename="src/categorywidget.cpp" line="109"/>
+        <source>None</source>
+        <translation>Intet</translation>
+    </message>
+</context>
+<context>
+    <name>EntryEditor</name>
+    <message>
+        <location filename="src/entryeditor.cpp" line="41"/>
+        <source>Barcode: </source>
+        <translation>Stregkode: </translation>
+    </message>
+    <message>
+        <location filename="src/entryeditor.cpp" line="51"/>
+        <source>New account, category or item?</source>
+        <oldsource>New account or item?</oldsource>
+        <translation>Ny konto, kategori eller vare?</translation>
+    </message>
+    <message>
+        <location filename="src/entryeditor.cpp" line="53"/>
+        <source>Account</source>
+        <translation>Konto</translation>
+    </message>
+    <message>
+        <location filename="src/entryeditor.cpp" line="58"/>
+        <source>Category</source>
+        <translation>Kategori</translation>
+    </message>
+    <message>
+        <location filename="src/entryeditor.cpp" line="63"/>
+        <source>Item</source>
+        <translation>Vare</translation>
+    </message>
+</context>
+<context>
+    <name>ItemEditor</name>
+    <message>
+        <location filename="src/itemeditor.cpp" line="41"/>
+        <source>Barcode: </source>
+        <translation>Stregkode: </translation>
     </message>
 </context>
 <context>
     <name>ItemWidget</name>
     <message>
-        <location filename="src/itemwidget.cpp" line="36"/>
+        <location filename="src/itemwidget.cpp" line="38"/>
         <source>Item name:</source>
         <translation>Vare navn:</translation>
     </message>
     <message>
-        <location filename="src/itemwidget.cpp" line="40"/>
+        <location filename="src/itemwidget.cpp" line="42"/>
         <source>Category:</source>
         <translation>Kategori:</translation>
     </message>
     <message>
-        <location filename="src/itemwidget.cpp" line="46"/>
+        <location filename="src/itemwidget.cpp" line="48"/>
+        <source>Icon:</source>
+        <translation>Billede:</translation>
+    </message>
+    <message>
+        <location filename="src/itemwidget.cpp" line="50"/>
+        <source>Icon search</source>
+        <translation>Billedsøgning</translation>
+    </message>
+    <message>
+        <location filename="src/itemwidget.cpp" line="55"/>
         <source>Price:</source>
         <translation>Pris:</translation>
+    </message>
+    <message>
+        <location filename="src/itemwidget.cpp" line="59"/>
+        <source>Discount:</source>
+        <translation>Rabat:</translation>
+    </message>
+    <message>
+        <location filename="src/itemwidget.cpp" line="128"/>
+        <source>None</source>
+        <translation>Intet</translation>
+    </message>
+</context>
+<context>
+    <name>ItemsModel</name>
+    <message>
+        <location filename="src/itemsmodel.cpp" line="149"/>
+        <source>Item</source>
+        <translation>Vare</translation>
+    </message>
+    <message>
+        <location filename="src/itemsmodel.cpp" line="152"/>
+        <source>Category</source>
+        <translation>Kategori</translation>
+    </message>
+    <message>
+        <location filename="src/itemsmodel.cpp" line="155"/>
+        <source>Price</source>
+        <translation>Pris</translation>
+    </message>
+    <message>
+        <location filename="src/itemsmodel.cpp" line="158"/>
+        <source>Discount</source>
+        <translation>Rabat</translation>
+    </message>
+    <message>
+        <location filename="src/itemsmodel.cpp" line="161"/>
+        <source>Stock</source>
+        <translation>På lager</translation>
+    </message>
+    <message>
+        <location filename="src/itemsmodel.cpp" line="164"/>
+        <source>Age</source>
+        <translation>Alder</translation>
+    </message>
+    <message>
+        <location filename="src/itemsmodel.cpp" line="167"/>
+        <source>Barcode</source>
+        <translation>Stregkode</translation>
+    </message>
+    <message>
+        <location filename="src/itemsmodel.cpp" line="170"/>
+        <source>Unknown</source>
+        <translation>Ukendt</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="src/mainwindow.cpp" line="131"/>
+        <location filename="src/mainwindow.cpp" line="137"/>
         <source>Barcode</source>
         <translation>Stregkode</translation>
     </message>
     <message>
-        <location filename="src/mainwindow.cpp" line="105"/>
+        <location filename="src/mainwindow.cpp" line="111"/>
         <source>&amp;About...</source>
         <translation>&amp;Om...</translation>
     </message>
     <message>
-        <location filename="src/mainwindow.cpp" line="112"/>
+        <location filename="src/mainwindow.cpp" line="68"/>
+        <source>Accounts</source>
+        <translation>Kontoer</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="69"/>
+        <source>Categories</source>
+        <translation>Kategorier</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="70"/>
+        <source>Items</source>
+        <translation>Varer</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="118"/>
         <source>&amp;File</source>
         <translation>&amp;Fil</translation>
     </message>
     <message>
-        <location filename="src/mainwindow.cpp" line="115"/>
+        <location filename="src/mainwindow.cpp" line="121"/>
         <source>&amp;Help</source>
         <translation>&amp;Hjælp</translation>
     </message>
     <message>
-        <location filename="src/mainwindow.cpp" line="141"/>
+        <location filename="src/mainwindow.cpp" line="147"/>
         <source>Main functions</source>
         <translation>Hovedfunktioner</translation>
-    </message>
-</context>
-<context>
-    <name>NewEntry</name>
-    <message>
-        <location filename="src/newentry.cpp" line="41"/>
-        <source>Barcode: </source>
-        <translation>Stregkode: </translation>
-    </message>
-    <message>
-        <location filename="src/newentry.cpp" line="48"/>
-        <source>New account or item?</source>
-        <translation>Ny konto eller vare?</translation>
-    </message>
-    <message>
-        <location filename="src/newentry.cpp" line="50"/>
-        <source>Account</source>
-        <translation>Konto</translation>
-    </message>
-    <message>
-        <location filename="src/newentry.cpp" line="56"/>
-        <source>Item</source>
-        <translation>Vare</translation>
     </message>
 </context>
 </TS>
