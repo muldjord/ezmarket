aa798b5c4f95f2cb74185d7f9eedeb29eafd2166
Lars Muldjord <muldjordlars@gmail.com>
2026-02-02 16:06:47 +0100

    German translation

---
+++ ezmarket_de.ts
@@ -0,0 +1,412 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="da_DK">
+<context>
+    <name>AboutBox</name>
+    <message>
+        <location filename="src/aboutbox.cpp" line="37"/>
+        <source>About EZMarket</source>
+        <oldsource>About EZLibrary</oldsource>
+        <translation>Über EZMarket</translation>
+    </message>
+    <message>
+        <location filename="src/aboutbox.cpp" line="70"/>
+        <source>&lt;h1&gt;EZMarket&lt;/h1&gt;&lt;h2&gt;Version </source>
+        <oldsource>&lt;h1&gt;EZLibrary&lt;/h1&gt;&lt;h2&gt;Version </oldsource>
+        <translation>&lt;h1&gt;EZMarket&lt;/h1&gt;&lt;h2&gt;Version </translation>
+    </message>
+</context>
+<context>
+    <name>AccountEditor</name>
+    <message>
+        <location filename="src/accounteditor.cpp" line="41"/>
+        <source>Barcode: </source>
+        <translation>Strichcode: </translation>
+    </message>
+    <message>
+        <location filename="src/accounteditor.cpp" line="73"/>
+        <source>Delete account?</source>
+        <translation>Konto löschen?</translation>
+    </message>
+    <message>
+        <location filename="src/accounteditor.cpp" line="73"/>
+        <source>Are you sure you want to delete this account?</source>
+        <translation>Sind Sie sicher, dass Sie dieses Kundenkonto löschen möchten?</translation>
+    </message>
+</context>
+<context>
+    <name>AccountWidget</name>
+    <message>
+        <location filename="src/accountwidget.cpp" line="39"/>
+        <source>Account holder:</source>
+        <translation>Kontoinhaber:</translation>
+    </message>
+    <message>
+        <location filename="src/accountwidget.cpp" line="43"/>
+        <source>Balance:</source>
+        <translation>Kontostand:</translation>
+    </message>
+    <message>
+        <location filename="src/accountwidget.cpp" line="46"/>
+        <source>Bonus points:</source>
+        <translation>Bonuspunkte:</translation>
+    </message>
+</context>
+<context>
+    <name>AccountsModel</name>
+    <message>
+        <location filename="src/accountsmodel.cpp" line="86"/>
+        <source>Barcode: </source>
+        <translation>Strichcode: </translation>
+    </message>
+    <message>
+        <location filename="src/accountsmodel.cpp" line="97"/>
+        <source>Account</source>
+        <translation>Kunde</translation>
+    </message>
+    <message>
+        <location filename="src/accountsmodel.cpp" line="100"/>
+        <source>Balance</source>
+        <translation>Kontostand</translation>
+    </message>
+    <message>
+        <location filename="src/accountsmodel.cpp" line="103"/>
+        <source>Bonus</source>
+        <translation>Bonus</translation>
+    </message>
+    <message>
+        <location filename="src/accountsmodel.cpp" line="111"/>
+        <source>Unknown</source>
+        <translation>Unbekannt</translation>
+    </message>
+</context>
+<context>
+    <name>ButtonGroup</name>
+    <message>
+        <location filename="src/buttongroup.cpp" line="34"/>
+        <source>Delete</source>
+        <translation>Löschen</translation>
+    </message>
+    <message>
+        <location filename="src/buttongroup.cpp" line="36"/>
+        <source>Save changes</source>
+        <translation>Änderungen speichern</translation>
+    </message>
+    <message>
+        <location filename="src/buttongroup.cpp" line="37"/>
+        <source>Cancel</source>
+        <translation>Stornieren</translation>
+    </message>
+</context>
+<context>
+    <name>CategoriesModel</name>
+    <message>
+        <location filename="src/categoriesmodel.cpp" line="75"/>
+        <source>Barcode: </source>
+        <translation>Strichcode: </translation>
+    </message>
+    <message>
+        <location filename="src/categoriesmodel.cpp" line="86"/>
+        <source>Category</source>
+        <translation>Kategorie</translation>
+    </message>
+    <message>
+        <location filename="src/categoriesmodel.cpp" line="89"/>
+        <source>Lifespan</source>
+        <translation>Lebensdauer (tage)</translation>
+    </message>
+    <message>
+        <location filename="src/categoriesmodel.cpp" line="97"/>
+        <source>Unknown</source>
+        <translation>Unbekannt</translation>
+    </message>
+</context>
+<context>
+    <name>CategoryEditor</name>
+    <message>
+        <location filename="src/categoryeditor.cpp" line="41"/>
+        <source>Barcode: </source>
+        <translation>Strichcode: </translation>
+    </message>
+    <message>
+        <location filename="src/categoryeditor.cpp" line="75"/>
+        <source>Delete category?</source>
+        <translation>Kategorie löschen?</translation>
+    </message>
+    <message>
+        <location filename="src/categoryeditor.cpp" line="75"/>
+        <source>Are you sure you want to delete this category?</source>
+        <translation>Sind Sie sicher, dass Sie dieses Kategorie löschen möchten?</translation>
+    </message>
+</context>
+<context>
+    <name>CategoryWidget</name>
+    <message>
+        <location filename="src/categorywidget.cpp" line="38"/>
+        <source>Category name:</source>
+        <translation>Kategoriename:</translation>
+    </message>
+    <message>
+        <location filename="src/categorywidget.cpp" line="42"/>
+        <source>Icon:</source>
+        <translation>Symbol:</translation>
+    </message>
+    <message>
+        <location filename="src/categorywidget.cpp" line="44"/>
+        <source>Icon search</source>
+        <translation>Symbolsuche</translation>
+    </message>
+    <message>
+        <location filename="src/categorywidget.cpp" line="49"/>
+        <source>Lifespan:</source>
+        <translation>Lebensdauer (tage):</translation>
+    </message>
+    <message>
+        <location filename="src/categorywidget.cpp" line="104"/>
+        <source>None</source>
+        <translation>Keiner</translation>
+    </message>
+</context>
+<context>
+    <name>Checkout</name>
+    <message>
+        <location filename="src/checkout.cpp" line="77"/>
+        <source>Paid by &apos;</source>
+        <translation>Bezahlt von &apos;</translation>
+    </message>
+    <message>
+        <location filename="src/checkout.cpp" line="79"/>
+        <source>Bonus for this purchase: </source>
+        <translation>Bonus für diesen Kauf: </translation>
+    </message>
+    <message>
+        <location filename="src/checkout.cpp" line="81"/>
+        <source>New balance: </source>
+        <translation>Neue Einzahlung: </translation>
+    </message>
+    <message>
+        <location filename="src/checkout.cpp" line="130"/>
+        <location filename="src/checkout.cpp" line="132"/>
+        <location filename="src/checkout.cpp" line="136"/>
+        <location filename="src/checkout.cpp" line="137"/>
+        <location filename="src/checkout.cpp" line="138"/>
+        <source>$</source>
+        <translation>€</translation>
+    </message>
+    <message>
+        <location filename="src/checkout.cpp" line="132"/>
+        <source> discount</source>
+        <translation> Rabatt</translation>
+    </message>
+    <message>
+        <location filename="src/checkout.cpp" line="136"/>
+        <source>Subtotal : </source>
+        <translation>Zwischensumme : </translation>
+    </message>
+    <message>
+        <location filename="src/checkout.cpp" line="137"/>
+        <source>Discount : </source>
+        <translation>Rabatt   : </translation>
+    </message>
+    <message>
+        <location filename="src/checkout.cpp" line="138"/>
+        <source>To pay   : </source>
+        <translation>Zu zahlender Betrag: </translation>
+    </message>
+    <message>
+        <location filename="src/checkout.cpp" line="145"/>
+        <source>Scan item...</source>
+        <translation>Artikel scannen...</translation>
+    </message>
+</context>
+<context>
+    <name>EntryEditor</name>
+    <message>
+        <location filename="src/entryeditor.cpp" line="41"/>
+        <source>Barcode: </source>
+        <translation>Strichcode: </translation>
+    </message>
+    <message>
+        <location filename="src/entryeditor.cpp" line="53"/>
+        <source>Account</source>
+        <translation>Kunde</translation>
+    </message>
+    <message>
+        <location filename="src/entryeditor.cpp" line="58"/>
+        <source>Category</source>
+        <translation>Kategorie</translation>
+    </message>
+    <message>
+        <location filename="src/entryeditor.cpp" line="63"/>
+        <source>Item</source>
+        <translation>Artikel</translation>
+    </message>
+</context>
+<context>
+    <name>ItemEditor</name>
+    <message>
+        <location filename="src/itemeditor.cpp" line="40"/>
+        <source>Barcode: </source>
+        <translation>Strichcode: </translation>
+    </message>
+    <message>
+        <location filename="src/itemeditor.cpp" line="72"/>
+        <source>Delete item?</source>
+        <translation>Artikel löschen?</translation>
+    </message>
+    <message>
+        <location filename="src/itemeditor.cpp" line="72"/>
+        <source>Are you sure you want to delete this item?</source>
+        <translation>Sind Sie sicher, dass Sie dieses Artikeln löschen möchten?</translation>
+    </message>
+</context>
+<context>
+    <name>ItemWidget</name>
+    <message>
+        <location filename="src/itemwidget.cpp" line="39"/>
+        <source>Item name:</source>
+        <translation>Artikelname:</translation>
+    </message>
+    <message>
+        <location filename="src/itemwidget.cpp" line="43"/>
+        <source>Category:</source>
+        <translation>Kategorie:</translation>
+    </message>
+    <message>
+        <location filename="src/itemwidget.cpp" line="49"/>
+        <source>Icon:</source>
+        <translation>Symbol:</translation>
+    </message>
+    <message>
+        <location filename="src/itemwidget.cpp" line="51"/>
+        <source>Icon search</source>
+        <translation>Symbolsuche</translation>
+    </message>
+    <message>
+        <location filename="src/itemwidget.cpp" line="56"/>
+        <source>Price:</source>
+        <translation>Preis:</translation>
+    </message>
+    <message>
+        <location filename="src/itemwidget.cpp" line="59"/>
+        <source>Discount:</source>
+        <translation>Rabatt:</translation>
+    </message>
+    <message>
+        <location filename="src/itemwidget.cpp" line="62"/>
+        <source>In stock:</source>
+        <translation>Auf Lager:</translation>
+    </message>
+    <message>
+        <location filename="src/itemwidget.cpp" line="134"/>
+        <source>None</source>
+        <translation>Keiner</translation>
+    </message>
+</context>
+<context>
+    <name>ItemsModel</name>
+    <message>
+        <location filename="src/itemsmodel.cpp" line="143"/>
+        <source>Barcode: </source>
+        <translation>Strichcode: </translation>
+    </message>
+    <message>
+        <location filename="src/itemsmodel.cpp" line="154"/>
+        <source>Item</source>
+        <translation>Artikel</translation>
+    </message>
+    <message>
+        <location filename="src/itemsmodel.cpp" line="157"/>
+        <source>Category</source>
+        <translation>Kategorie</translation>
+    </message>
+    <message>
+        <location filename="src/itemsmodel.cpp" line="160"/>
+        <source>Price</source>
+        <translation>Preis</translation>
+    </message>
+    <message>
+        <location filename="src/itemsmodel.cpp" line="163"/>
+        <source>Discount</source>
+        <translation>Rabatt</translation>
+    </message>
+    <message>
+        <location filename="src/itemsmodel.cpp" line="166"/>
+        <source>Stock</source>
+        <translation>Lagerbestand</translation>
+    </message>
+    <message>
+        <location filename="src/itemsmodel.cpp" line="169"/>
+        <source>Age</source>
+        <translation>Alter</translation>
+    </message>
+    <message>
+        <location filename="src/itemsmodel.cpp" line="177"/>
+        <source>Unknown</source>
+        <translation>Unbekannt</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="src/mainwindow.cpp" line="130"/>
+        <source>Barcode</source>
+        <translation>Strichcode</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="105"/>
+        <source>&amp;About...</source>
+        <translation>&amp;Über...</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="67"/>
+        <source>Accounts</source>
+        <translation>Konten</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="68"/>
+        <source>Categories</source>
+        <translation>Kategorien</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="69"/>
+        <source>Items</source>
+        <translation>Artikel</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="70"/>
+        <source>Announcements</source>
+        <translation>Ankündigungen</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="71"/>
+        <source>Checkout</source>
+        <translation>Kasse</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="112"/>
+        <source>&amp;File</source>
+        <translation>&amp;Datei</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="115"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Helfen</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="146"/>
+        <source>Main functions</source>
+        <translation>Hauptfunktionen</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="558"/>
+        <source>Click to open the store</source>
+        <translation>Klicken Sie hier, um den Shop zu öffnen</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.cpp" line="569"/>
+        <source>Click to close the store</source>
+        <translation>Klicken Sie hier, um den Shop zu schließen</translation>
+    </message>
+</context>
+</TS>
